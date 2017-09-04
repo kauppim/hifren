10bafbb5c48259e23ef29bbb7fb9856ed19c1f6d
Mikael Kauppinen <mikael.kauppinen@hiq.fi>
2017-09-04 11:30:55 +0300

    This commit includes all the work done during the personal project day of HiQ Finland's IEX Team on 25 Aug 2017.

--- Hifren/Hifren.Tests/TestProgram.cs
+++ Hifren/Hifren.Tests/TestProgram.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
+using Hifren.Client;
 
 namespace Hifren.Tests
 {
@@ -10,6 +12,41 @@ namespace Hifren.Tests
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("START Chat server instance...");
+            Console.WriteLine("=============================");
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                Hifrend.MsgServer("Monkey");
+            }).Start();
+
+
+            var demo1 = new Blabla();
+            var demo2 = new Blabla();
+
+
+            Console.WriteLine("START Chat client demo 1...");
+            Console.WriteLine("=============================");
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                demo1.ChatClient("tcp://127.0.0.1:5555", "Demo1", "Hello");
+            }).Start();
+
+
+            Console.WriteLine("START Chat client demo 2...");
+            Console.WriteLine("=============================");
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                demo2.ChatClient("tcp://127.0.0.1:5555", "Demo2", "Bye");
+            }).Start();
+
+
+            Thread.Sleep(2000);
+            Console.WriteLine("=============================> Ready.");
+            Console.ReadLine();
+
         }
     }
 }
